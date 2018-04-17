e592e1a444617179b1af16231ddd800c94c78b50
Alex Noble and Jimmy McNamara <alex@>
2018-04-17 09:01:17 -0700

    add list, clear function, and delete

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,22 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
     public class ItemsController : Controller
     {
 
-        [HttpGet("/items")]
-        public ActionResult Index()
-        {
-            Item newItem = new Item(Request.Query["new-item"]);
-            return View(newItem);
-        }
+      [HttpGet("/items")]
+      public ActionResult Index()
+      {
+          List<Item> allItems = Item.GetAll();
+          return View(allItems);
+      }
 
-        [HttpGet("/items/new")]
-        public ActionResult CreateForm()
-        {
-            return View();
-        }
+      [HttpGet("/items/new")]
+      public ActionResult CreateForm()
+      {
+          return View();
+      }
+      [HttpPost("/items")]
+      public ActionResult Create()
+      {
+        Item newItem = new Item (Request.Form["new-item"]);
+        newItem.Save();
+        List<Item> allItems = Item.GetAll();
+        return View("Index", allItems);
+      }
+
+      [HttpPost("/items/delete")]
+      public ActionResult DeleteAll()
+      {
+          Item.ClearAll();
+          return View();
+      }
     }
 }
--- Models/Item.cs
+++ Models/Item.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
     private string _description;
+    private static List<Item> _instances = new List<Item> {};
+
     public Item (string description)
     {
       _description = description;
@@ -14,6 +18,18 @@ namespace ToDoList.Models
     public void SetDescription(string newDescription)
     {
       _description = newDescription;
+    }
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+    public void Save()
+    {
+      _instances.Add(this);
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
